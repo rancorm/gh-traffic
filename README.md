7a27c2e1e6103b65272179f76d9077894eb1ddcb
Jonathan Cormier <jonathan@cormier.co>
2025-05-06 17:37:24 -0300

    feat: add initial implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+# gh-traffic
+
+GitHub CLI extension to display repository traffic (clones & views) from a terminal.
--- main.go
+++ main.go
@@ -1,26 +1,94 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
-
+	"os"
+	"github.com/cli/go-gh/v2/pkg/repository"
 	"github.com/cli/go-gh/v2/pkg/api"
 )
 
-func main() {
-	fmt.Println("hi world, this is the gh-traffic extension!")
-	client, err := api.DefaultRESTClient()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
+// Create a combined struct for JSON output
+type TrafficData struct {
+	Repository string `json:"repository"`
+	Views struct {
+		Count   int `json:"count"`
+		Uniques int `json:"uniques"`
+	} `json:"views"`
+	Clones struct {
+		Count   int `json:"count"`
+		Uniques int `json:"uniques"`
+	} `json:"clones"`
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func main() {
+	owner := flag.String("owner", "", "Repository owner")
+	repo := flag.String("repo", "", "Repository name")
+	outputJSON := flag.Bool("json", false, "Output as JSON")
+	
+	flag.Parse()
+
+	var repoOwner, repoName string
+
+	if *owner != "" && *repo != "" {
+		repoOwner = *owner
+		repoName = *repo
+	} else {
+		repoCtx, err := repository.Current()
+			
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Could not determine repository context:", err)
+			os.Exit(1)
+		}
+		
+		repoOwner = repoCtx.Owner
+		repoName = repoCtx.Name
+	}
+
+	client, err := api.DefaultRESTClient()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating REST client: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Fetch views
+	var views struct {
+		Count     int `json:"count"`
+		Uniques   int `json:"uniques"`
+	}
+	
+	err = client.Get(fmt.Sprintf("repos/%s/%s/traffic/views", repoOwner, repoName), &views)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching views: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Fetch clones
+	var clones struct {
+		Count     int `json:"count"`
+		Uniques   int `json:"uniques"`
+	}
+	
+	err = client.Get(fmt.Sprintf("repos/%s/%s/traffic/clones", repoOwner, repoName), &clones)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching clones: %v\n", err)
+		os.Exit(1)
+	}
+	
+	// After fetching repo details, build traffic data struct.
+	data := TrafficData{
+		Repository: fmt.Sprintf("%s/%s", repoOwner, repoName),
+		Views:      views,
+		Clones:     clones,
+	}
+
+	// Output formatting
+	if *outputJSON {
+		json.NewEncoder(os.Stdout).Encode(data)
+	} else {
+		fmt.Printf("Repository: %s\n", data.Repository)
+		fmt.Printf("    Clones: %d total, %d unique\n", data.Clones.Count, data.Clones.Uniques)
+		fmt.Printf("  Visitors: %d total, %d unique\n", data.Views.Count, data.Views.Uniques)
+	}
+}
